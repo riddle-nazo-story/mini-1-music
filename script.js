2abdb36a72be0b873eee0903e16bfef42f42dcdf
riddle-nazo-story <yumeda0107@gmail.com>
2026-03-12 00:59:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ function playSE(type){
 
 const audio = new Audio("audio/"+type+".mp3")
 
-audio.currentTime=0
+audio.currentTime = 0
 
 audio.play()
 
 }
 
 
-// 音量変更
+// 音量変更（スマホ対応）
 
 function setVolume(id,value){
 
-document.getElementById(id).volume=value
+const audio = document.getElementById(id)
+
+audio.volume = parseFloat(value)
 
 }
 
 
 // タイマー
 
-let time=900
-let timer=null
+let time = 900
+let timer = null
 
 function updateDisplay(){
 
-let m=Math.floor(time/60)
-let s=time%60
+let m = Math.floor(time/60)
+let s = time%60
 
 if(s<10)s="0"+s
 
-document.getElementById("time").innerText=m+":"+s
+document.getElementById("time").innerText = m + ":" + s
 
 }
 
@@ -40,7 +42,7 @@ function startTimer(){
 
 if(timer)return
 
-timer=setInterval(()=>{
+timer = setInterval(()=>{
 
 time--
 
